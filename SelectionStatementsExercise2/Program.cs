302f685a37a488090ef689c33ed3fe40456f342f
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-21 12:18:51 -0700

    Completed Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 2:
+
+            Console.WriteLine("What is your favorite school subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is hard!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is intersting!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is exhausting!");
+                    break;
+                case "recess":
+                    Console.WriteLine("Recess is BEST!");
+                    break;
+                default:
+                    Console.WriteLine($"{favSubject} sounds like a blast!");
+                    break;
+            }
         }
     }
 }
